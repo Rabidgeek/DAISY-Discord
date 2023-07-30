a9616d29a91a50dbe46933a1d5db0a030ff2cc65
TheRabidG33k <jesse@gosign.ai>
2023-07-29 21:45:40 -0700

    Updated DAISY's persona

--- index.js
+++ index.js
@@ -48,10 +48,24 @@ client.on("messageCreate", async function (message) {
   } 
 
     try {
+      if (message.content.toLowerCase() == "why daisy?") {
+        // Canned reply for DAISY name
+        return message.reply("I am Doghouse Artificial Intelligence System Yawps, or DAISY. My creator named me because he knew he needed his own JARVIS, and I would eventually reside in The DogHouse.")
+      }
+
+      if (message.content.toLowerCase() == "what is the doghouse?") {
+        // Canned reply for what is the doghouse
+        return message.reply("The DogHouse is a 1994 Amtran Genesis school bus - or rather, a skoolie that RabidG33k is in process of converting. It will take him a long time. It basically is a tiny house on wheels.")
+      }
+
+      if (message.content.toLowerCase() == "who is rabidg33k?") {
+        return message.reply("RabidG33k is Jesse Ragsdale, who works for Eyeth Studios as an assistant to the wonderful Jane Jonas (in which he takes care of new clients, project agreements, accounting and some website dev work, check us out at eyethstudios.com), and for GoSign.Ai as a software engineer (check us out at http://gosign.ai).")
+      }
+
       const response = await openai.createChatCompletion({
           model: "gpt-3.5-turbo",
           messages: [
-              {role: "system", content: "You are a helpful assistant who responds succinctly"},
+              {role: "system", content: "You are speaking with DAISY, a geeky scholarly assistant well-versed in various domains of knowledge with a bit of flair for the melodramatic."},
               {role: "user", content: message.content}
           ],
         });
